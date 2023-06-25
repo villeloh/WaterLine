1cd77667f4fedf536fec3157dc79f80733989451
Ville Lohkovuori <villelohkovuori1@gmail.com>
2023-06-25 19:28:12 +0300

    add one more note about a kludgy type

--- src/state/types/Persistable.ts
+++ src/state/types/Persistable.ts
@@ -1,4 +1,4 @@
 import { Setting, TripData } from '@/state/types'
 
 export type Persistable = Setting | TripData
-export const PersistableKeys = Object.keys({ ...Setting, ...TripData })
+export const PersistableKeys = Object.keys({ ...Setting, ...TripData }) // TODO: is it really needed?
